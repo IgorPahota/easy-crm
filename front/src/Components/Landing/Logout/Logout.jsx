d8533052b1127fc8fb7a8ff6f51eb052cdeb8114
Igor Pahota <eto.pochtovaya@gmail.com>
2020-01-24 13:30:15 +0300

    added logout correct button

--- front/src/Components/Landing/Logout/Logout.jsx
+++ front/src/Components/Landing/Logout/Logout.jsx
@@ -1,19 +1,33 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux'
+import {loggedOut} from "../../../redux/loggedOut";
 
 class Logout extends Component {
 
     logoutFetch = async () => {
         let response = await fetch('/logout');
         let result = await response.json();
+        if (!result.isLoggedIn) {
+            this.props.set();
+            alert('you are logged out')
+        } else alert(result.error)
     };
 
     render() {
         return (
             <div>
-                <button>Logout</button>
+                <button onClick={this.logoutFetch}>Logout</button>
             </div>
         );
     }
 }
 
-export default Logout;
+function mapDispatchToProps (dispatch) {
+    return {
+        set: () => {
+            dispatch(loggedOut())
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Logout);
