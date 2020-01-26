0d76748b491dbc88c73bbd9c5ff675fee8825046
AVBelyaef <avbelyaef@ya.ru>
2020-01-26 23:15:56 +0300

    add link

--- front/src/Components/Application/Dashboard/Dashboard.jsx
+++ front/src/Components/Application/Dashboard/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
 
 class Dashboard extends Component {
 
   state = {
     leads: []
+
   };
 
   componentDidMount = async () => {
@@ -13,12 +16,24 @@ class Dashboard extends Component {
   };
 
   render() {
+    let sum = 0;
+    const {leads} = this.state
+    leads.map(lead => sum += lead.price);
+    const numberOfLeads = leads.length;
     return (
       <div>
-
+        <h3>Your leads</h3>
+        <p>Общая сумма: { sum }</p>
+        <p> Всего сделок: { numberOfLeads } </p>
+        {leads.map((lead) =>
+          <div key={lead.id}>
+            <Link to={`/lead/${lead.id}`}>{lead.name}</Link>
+          </div>
+        )}
       </div>
     );
   }
 }
 
+// export default connect(mapStateToProps)(Dashboard);
 export default Dashboard;
