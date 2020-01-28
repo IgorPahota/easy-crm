404aacf1799cb23459dcbcdc9be5bd857f6bfe85
Pavel Andras <pavel.andras.pa@gmail.com>
2020-01-28 13:38:21 +0300

    minimum design for leads page

--- front/src/App.js
+++ front/src/App.js
@@ -1,77 +1,79 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux'
-import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
-import { ConfigProvider } from 'antd';
-import ruRU from 'antd/es/locale/ru_RU';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import { ConfigProvider } from "antd";
+import ruRU from "antd/es/locale/ru_RU";
 import Login from "./Components/Landing/Login/Login";
 import Signup from "./Components/Landing/Signup/Signup";
 import Dashboard from "./Components/Application/Dashboard/Dashboard";
-import ContactsList from './Components/Application/Contacts/ContactsList';
+import ContactsList from "./Components/Application/Contacts/ContactsList";
 import LandingNavbar from "./Components/Landing/LandingNavbar/LandingNavbar";
-import {loggedIn} from "./redux/loggedIn";
+import { loggedIn } from "./redux/loggedIn";
 import ApplicationNavbar from "./Components/Application/ApplicationNavbar/ApplicationNavbar";
-import './App.css';
-import ContactInfo from './Components/Application/Contacts/ContactInfo';
+import "./App.css";
+import ContactInfo from "./Components/Application/Contacts/ContactInfo";
 import Leads from "./Components/Application/Leads/Leads";
 
-
 class App extends Component {
-    componentDidMount = async () =>  {
-        let response = await fetch('/login');
-        let result = await response.json();
-        if (result.isLoggedIn) {
-            const responseContacts = await fetch('/contacts');
-            const contacts = await responseContacts.json();
+  componentDidMount = async () => {
+    let response = await fetch("/login");
+    let result = await response.json();
+    if (result.isLoggedIn) {
+      const responseContacts = await fetch("/contacts");
+      const contacts = await responseContacts.json();
 
-            // alert('you already logged in');
-            let arrayWithProps = [result.username, result.email, result.id, contacts];
-            this.props.set(arrayWithProps)
-        } else {
-            // alert('login please')
-        }
-    };
-    render() {
-
-        return (
-          <ConfigProvider locale={ruRU}>
-            <Router>
-            <div className="App">
-                APP.JS
-                {!this.props.isLoggedIn && <LandingNavbar/>}
-                {this.props.isLoggedIn && <ApplicationNavbar/>}
-                <Switch>
-                    <Route  path='/login' component={Login}/>
-                    <Route  path='/signup' component={Signup}/>
-                    <Route  path='/dashboard' component={Dashboard}/>
-                    <Route exact path='/contacts' component={ContactsList}/>
-                    <Route path="/contacts/:id" component={ContactInfo} />
-                    <Route path="/leads" component={Leads}/>
-                    {/*<Route render={()=>{*/}
-                    {/*    return (*/}
-                    {/*        <Redirect to={'/login'}/>*/}
-                    {/*    )*/}
-                    {/*}}/>*/}
-                </Switch>
-            </div>
-            </Router>
-          </ConfigProvider>
-        );
+      // alert('you already logged in');
+      let arrayWithProps = [result.username, result.email, result.id, contacts];
+      this.props.set(arrayWithProps);
+    } else {
+      // alert('login please')
     }
+  };
+  render() {
+    return (
+      <ConfigProvider locale={ruRU}>
+        <Router>
+          <div className="App">
+            APP.JS
+            {!this.props.isLoggedIn && <LandingNavbar />}
+            {this.props.isLoggedIn && <ApplicationNavbar />}
+            <Switch>
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route exact path="/contacts" component={ContactsList} />
+              <Route path="/contacts/:id" component={ContactInfo} />
+              <Route path="/leads" component={Leads} />
+              {/*<Route render={()=>{*/}
+              {/*    return (*/}
+              {/*        <Redirect to={'/login'}/>*/}
+              {/*    )*/}
+              {/*}}/>*/}
+            </Switch>
+          </div>
+        </Router>
+      </ConfigProvider>
+    );
+  }
 }
 
 function mapStateToProps(store) {
-    return {
-        isLoggedIn: store.isLoggedIn
-    }
+  return {
+    isLoggedIn: store.isLoggedIn
+  };
 }
 
-function mapDispatchToProps (dispatch) {
-    return {
-        set: (data) => {
-            dispatch(loggedIn(data))
-        }
+function mapDispatchToProps(dispatch) {
+  return {
+    set: data => {
+      dispatch(loggedIn(data));
     }
+  };
 }
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- front/src/Components/Application/Leads/Leads.jsx
+++ front/src/Components/Application/Leads/Leads.jsx
@@ -2,132 +2,153 @@ import React, { Component } from "react";
 import Board from "react-trello";
 
 export default class Leads extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: {
-                lanes: [
-                    {
-                        id: undefined,
-                        title: undefined,
-                        cards: [
-                            {
-                                id: undefined,
-                                title: undefined,
-                                description: undefined
-                            }
-                        ]
-                    }
-                ]
-            }
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        lanes: [
+          {
+            id: undefined,
+            title: undefined,
+            cards: [
+              {
+                id: undefined,
+                title: undefined,
+                description: undefined
+              }
+            ]
+          }
+        ]
+      }
+    };
+  }
 
-    componentDidMount = async () => {
-        let response = await fetch("/stages");
-        let result = await response.json();
-        let responseLead = await fetch("/leads");
-        let resultLeads = await responseLead.json();
-        const lanes = [];
-        const cards = [];
+  componentDidMount = async () => {
+    let response = await fetch("/stages");
+    let result = await response.json();
+    let responseLead = await fetch("/leads");
+    let resultLeads = await responseLead.json();
+    const lanes = [];
+    const cards = [];
 
-        resultLeads.map(element => {
-            // console.log(element);
-            cards.push({
-                id: element._id,
-                title: element.name,
-                description: element.details,
-                stageId: element.stageID
-            });
-        });
+    resultLeads.map(element => {
+      // console.log(element);
+      cards.push({
+        id: element._id,
+        title: element.name,
+        description: element.details,
+        stageId: element.stageID
+      });
+    });
 
-        result.map(element => {
-            // console.log(element);
-            lanes.push({
-                id: element._id,
-                title: element.name,
-                cards: cards
-            });
-        });
+    result.map(element => {
+      // console.log(element);
+      lanes.push({
+        id: element._id,
+        title: element.name,
+        cards: cards
+      });
+    });
 
-        this.setState({
-            data: {
-                lanes
-            }
-        });
+    this.setState({
+      data: {
+        lanes
+      }
+    });
+  };
+
+  onLaneAdd = async params => {
+    // console.log(params);
+    let response = await fetch("/stages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: params.title
+      })
+    });
+    // let result = await response.json();
+    // console.log(result);
+  };
+
+  onLaneDelete = async params => {
+    // console.log(params);
+    let response = await fetch(`/stages/${params}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        id: params.id
+      })
+    });
+  };
+
+  onCardAdd = async params => {
+    console.log(params);
+
+    let response = await fetch("/leads", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: params.title,
+        details: params.description
+      })
+    });
+  };
+
+  onCardDelete = async params => {
+    alert("deleting card");
+  };
+
+  render() {
+    const fontfamily =
+      "font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'";
+    const style = {
+      backgroundColor: "white",
+      fontFamily: fontfamily
+    };
+    const laneStyle = {
+      backgroundColor: "white",
+      border: "1px solid black",
+      fontFamily: fontfamily,
+      //   fontWeight: "bold",
+      borderRadius: 0
+    };
+    const cardStyle = {
+      border: "1px solid black",
+      fontFamily: fontfamily,
+      borderRadius: 0
     };
 
-    onLaneAdd = async params => {
-        // console.log(params);
-        let response = await fetch("/stages", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name: params.title
-            })
-        });
-        // let result = await response.json();
-        // console.log(result);
-    };
-
-    onLaneDelete = async params => {
-        // console.log(params);
-        let response = await fetch(`/stages/${params}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                id: params.id
-            })
-        });
-    };
-
-    onCardAdd = async params => {
-        console.log(params);
-
-        let response = await fetch("/leads", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name: params.title,
-                details: params.description
-            })
-        });
-    };
-
-    onCardDelete = async params => {
-        alert("deleting card");
-    };
-
-    render() {
-        return (
-            <div>
-                {!this.state.data ? (
-                    <div>LoADING</div>
-                ) : (
-                    <Board
-                        data={this.state.data}
-                        /*data={data}*/
-                        editable
-                        laneDraggable={false}
-                        canAddLanes
-                        editLaneTitle
-                        onLaneAdd={this.onLaneAdd}
-                        onLaneDelete={this.onLaneDelete}
-                        onCardDelete={this.onCardDelete}
-                        onCardAdd={
-                            this.onCardAdd
-                        } /*
-  components={{Card: CustomCard}}
-  onCardClick={onCardClick} */
-                    />
-                )}
-            </div>
-        );
-    }
+    return (
+      <div>
+        {!this.state.data ? (
+          <div>LoADING</div>
+        ) : (
+          <Board
+            data={this.state.data}
+            /*data={data}*/
+            editable
+            laneDraggable={false}
+            canAddLanes
+            editLaneTitle
+            onLaneAdd={this.onLaneAdd}
+            onLaneDelete={this.onLaneDelete}
+            onCardDelete={this.onCardDelete}
+            onCardAdd={this.onCardAdd}
+            style={style}
+            laneStyle={laneStyle}
+            cardStyle={cardStyle}
+            /*
+      components={{Card: CustomCard}}
+      onCardClick={onCardClick} */
+          />
+        )}
+      </div>
+    );
+  }
 }
