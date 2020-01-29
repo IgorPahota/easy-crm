af4fd3c0c49b03cdaa4a008f13a4b0644cea70fc
AVBelyaef <avbelyaef@ya.ru>
2020-01-29 18:50:42 +0300

    add chart

--- front/src/Components/Application/Dashboard/Dashboard.jsx
+++ front/src/Components/Application/Dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 import { Modal, Row, Col, Alert, Spin, Card, Button } from 'antd';
+import Chart from "./Chart";
+// import insertCss from './insert-css.css';
 
 
 class Dashboard extends Component {
@@ -36,12 +38,16 @@ class Dashboard extends Component {
   componentDidMount = async () => {
     let responseStages = await fetch('/stages');
     let stages = await responseStages.json();
-    this.setState({ stages })
+    this.setState({ stages });
+
+
   };
 
   render() {
 
     const { stages } = this.state;
+    let allLeadsPrice = 0;
+    let allLeads = 0;
 
     return (
       <div>
@@ -61,18 +67,18 @@ class Dashboard extends Component {
               <Row gutter={16} style={{ overflowX: 'auto ', display: 'flex' }}>
                 {stages.map((stage) => {
                     let sumStages = 0;
+                  allLeads += stage.cards.length
                     stage.cards.forEach(card => {
                       sumStages += card.price;
-                    });
+                      allLeadsPrice += card.price;
 
+                    });
                     return (
                       <Col span={6} key={stage._id} onClick={() => this.showModal(stage)}
                            style={{ marginBottom: '30px', width: 'auto' }}>
-
                         <Card title={stage.title} bordered={false} style={{ marginTop: '8px' }}>
                           <p>Количество сделок: {stage.cards.length}</p>
                           <p>На сумму: {sumStages} руб.</p>
-
                         </Card>
                       </Col>
                     );
@@ -80,6 +86,9 @@ class Dashboard extends Component {
                 )}
               </Row>
             </div>
+              <p>Сумма всех сделок:{allLeadsPrice}</p>
+              <p>Количество всех сделок:{allLeads}</p>
+              {/*<p>Количество завершеных сделок:{allSum}</p>*/}
 
             <div>
               <Modal
@@ -90,14 +99,14 @@ class Dashboard extends Component {
                 footer={null}
               >
                 {this.state.stage.cards && this.state.stage.cards.map(card =>
-                  // <p> {card.name}</p>
                   <p><Link to={`/lead/${card._id}`}>{card.name}</Link> на сумму: {card.price} </p>
                 )}
               </Modal>
             </div>
-
+            <Chart />
           </>
         }
+
       </div>
     );
   }
